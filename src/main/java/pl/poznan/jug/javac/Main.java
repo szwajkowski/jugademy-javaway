c12289e904e0dc0d08a30f71d96def4c47654c16
Lukasz Szwajkowski <lukasz.szwajkowski@allegrogroup.com>
2017-01-11 21:44:49 +0100

    formatting

--- src/main/java/pl/poznan/jug/javac/Main.java
+++ src/main/java/pl/poznan/jug/javac/Main.java
@@ -2,7 +2,7 @@ package pl.poznan.jug.javac;
 
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("Hello Javac!");
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello Javac!");
+    }
 }
